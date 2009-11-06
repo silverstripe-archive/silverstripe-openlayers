bd15ba74ba76421ccab4d4f4047b4187487d0b80
cbarberis <cbarberis@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-11-06 01:19:33 +0000

    DataObject to store a Map Properties
    git-svn-id: svn://svn.silverstripe.com/silverstripe/modules/openlayers/trunk@90945 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ code/OLMapProperty.php
@@ -0,0 +1,15 @@
+<?php
+
+class OLMapProperty extends DataObject {
+	
+	static $db = array(
+		'Name' => 'Varchar(255)',
+		'Value' => 'Varchar(255)'
+	);
+	
+	static $has_one = array(
+		'Map' => 'OLMapPage'
+	);
+	
+}
+?>
