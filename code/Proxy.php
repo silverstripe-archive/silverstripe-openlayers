5d166d26b8b3dd72a7a72b726aa8e8dd37a82194
rspittel <rspittel@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-11-23 02:58:11 +0000

    BUGFIX: added post-body support for WFS requests.
    git-svn-id: svn://svn.silverstripe.com/silverstripe/modules/openlayers/trunk@93020 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/Proxy.php
+++ code/Proxy.php
@@ -12,24 +12,42 @@ class Proxy_Controller extends Controller {
 	
 	public function dorequest($data) {
 		$vars = $data->getVars();
+		$headers = array();
 		
 		$url = $vars['u'];
+		
 		$checkUrl = explode("/",$url);
-		if(!in_array($checkUrl[2],self::$allowedHost)) user_error("This proxy does not allow you to access that location ($url).", E_USER_ERROR);
-		$isPost = $data->isPOST();
-				
+		if(!in_array($checkUrl[2],self::$allowedHost)) {
+			user_error("This proxy does not allow you to access that location ($url).", E_USER_ERROR);
+		}
+		
 		// Open the Curl session
 		$session = curl_init($url);
 
 		// If it's a POST, put the POST data in the body
-		if ($data->isPOST()) {
+		$isPost = $data->isPOST();
+		if ($isPost) {
 			$postvars = '';
-			$vars = $data->postVars();
-			foreach($vars as $k => $v) {
-				$postvars .= $k.'='.$v.'&';
+			$vars = $data->getBody();
+			
+			if ($vars) {
+				$postvars = "body=".$vars;
+			} else {
+				$vars = $data->postVars();
+
+				if ($vars) {
+					foreach($vars as $k => $v) {
+						$postvars .= $k.'='.$v.'&';
+					}
+				}
 			}
+			
+			$headers[] = 'Content-type: text/xml';
+			curl_setopt ($session, CURLOPT_HTTPHEADER, $headers); 
+			
 			curl_setopt ($session, CURLOPT_POST, true);
 			curl_setopt ($session, CURLOPT_POSTFIELDS, $postvars);
+
 		}
 
 		// Don't return HTTP headers. Do return the contents of the call
