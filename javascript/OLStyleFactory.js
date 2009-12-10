832d40ee6ae5998e998756fca89f4909de997fe2
rspittel <rspittel@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-12-10 03:14:36 +0000

    MINOR: added different map styles for all layers.
    git-svn-id: svn://svn.silverstripe.com/silverstripe/modules/openlayers/trunk@94851 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/OLStyleFactory.js
+++ javascript/OLStyleFactory.js
@@ -9,13 +9,38 @@ OLStyleFactory.createStyleMap = function (layerName) {
 	var styleMap = null;
 
 	if (layerName == 'DTIS') {
-		styleMap = this.createStyleMap_DTIS();
+		styleMap = this.createStyleMap_Point("#FFCC66", "#cc6633");
 	} else
 	if (layerName == 'CTD') {
 		styleMap = this.createStyleMap_CTD();
+	} else
+	if (layerName == 'Bottom_grab') {
+		styleMap = this.createStyleMap_Point("#CCFF66", "#606060");
+	} else
+	if (layerName == 'Multicorer') {
+		styleMap = this.createStyleMap_Point("#CC66FF", "#606060");
+	} else
+	if (layerName == 'Sediment_sample') {
+		styleMap = this.createStyleMap_Point("#FF66CC", "#606060");
+	} else
+	if (layerName == 'Beam_trawl') {
+		// line
+		styleMap = this.createStyleMap_Line("#202020", 4, 0.8);
+	} else
+	if (layerName == 'Brenke_sled') {
+		// line
+		styleMap = this.createStyleMap_Line("#602020", 4, 0.8);
+	} else
+	if (layerName == 'Dredge') {
+		// line
+		styleMap = this.createStyleMap_Line("#206020", 4, 0.8);
+	} else
+	if (layerName == 'Epibenthic_sled') {
+		// line
+		styleMap = this.createStyleMap_Line("#202060", 4, 0.8);
 	} else {
 		// default style
-		styleMap = this.createStyleMap_Default();
+		styleMap = this.createStyleMap_Point("#FFCC66", "#cc6633");
 	}
 	return styleMap;
 }
@@ -23,51 +48,15 @@ OLStyleFactory.createStyleMap = function (layerName) {
 /**
  * Return a specific style map for CTD vector data
  */
-OLStyleFactory.createStyleMap_Default = function() {
+OLStyleFactory.createStyleMap_Point = function(fillColor, strokeColor) {
 	
-	default_style = new OpenLayers.Style({
+	var default_style = new OpenLayers.Style({
         pointRadius: "${radius}",
-        fillColor: "#ffcc66",
+        fillColor: fillColor,
         fillOpacity: 0.8,
-        strokeColor: "#cc6633",
+        strokeColor: strokeColor,
         strokeWidth: 2,
-        strokeOpacity: 0.8
-    }, {
-        context: {
-            radius: function(feature) {
-                return Math.min(feature.attributes.count, 7) + 3;
-            }
-        }
-    });
-
-	// create style map class
-	var styleMap = new OpenLayers.StyleMap({
-        "default": default_style,
-        "select": {
-            fillColor: "#8aeeef",
-            strokeColor: "#32a8a9"
-        },
-		"temporary" : {
-			fontColor: "#000000",
-	        fillColor: "#ff6666",
-            strokeColor: "#f00000"
-		}
-    });
-	return styleMap;
-}
-
-/**
- * Return a specific style map for DTIS vector data
- */
-OLStyleFactory.createStyleMap_DTIS = function() {
-	
-	default_style = new OpenLayers.Style({
-		pointRadius: "${radius}",
-		fillColor: "#ffcc66",
-		fillOpacity: 0.8,
-		strokeColor: "#cc6633",
-		strokeWidth: 2,
-		strokeOpacity: 0.8	,	       
+        strokeOpacity: 0.8,
 		label: "${title}", 
 		fontColor: "black"
     }, {
@@ -94,7 +83,34 @@ OLStyleFactory.createStyleMap_DTIS = function() {
             strokeColor: "#f00000"
 		}
     });
+	return styleMap;
+}
 
+/**
+ * 
+ */
+OLStyleFactory.createStyleMap_Line = function(strokeColor,strokeWidth,strokeOpacity) {
+	
+	var default_style = new OpenLayers.Style({
+        strokeColor: strokeColor,
+        strokeWidth: strokeWidth,
+        strokeOpacity: strokeOpacity,
+    });
+
+
+	// create style map class
+	var styleMap = new OpenLayers.StyleMap({
+        "default": default_style,
+        "select": {
+            fillColor: "#8aeeef",
+            strokeColor: "#32a8a9"
+        },
+		"temporary" : {
+			fontColor: "#000000",
+	        fillColor: "#ff6666",
+            strokeColor: "#f00000"
+		}
+    });
 	return styleMap;
 }
 
