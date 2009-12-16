40fb74ea28268a90f80df020345d25a259966ddb
rspittel <rspittel@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-12-16 00:02:42 +0000

    MINOR: added documentation
    git-svn-id: svn://svn.silverstripe.com/silverstripe/modules/openlayers/trunk@95498 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/model/OpenLayersModel.php
+++ code/model/OpenLayersModel.php
@@ -15,19 +15,9 @@ class OpenLayersModel {
 	
 	function __construct() {
 		$this->allowedHosts = array();
-		$this->allowedHosts[] = '202.36.29.39';
-		
-	/*	
-	    host = url.split("/")[2]
-	    if allowedHosts and not host in allowedHosts:
-	        print "Status: 502 Bad Gateway"
-	        print "Content-Type: text/plain"
-	        print
-	        print "This proxy does not allow you to access that location (%s)." % (host,)
-	        print
-	        print os.environ
-	*/		
+		$this->allowedHosts[] = '202.36.29.39';	
 	}
+	
 	/**
 	 * Returns the relative URL to the OpenLayers JavaScript library.
 	 *
@@ -41,11 +31,32 @@ class OpenLayersModel {
 		}
 		return $openlayerJS;
 	}
-	
-	function getAllowedHosts() {
-		
-		var_dump($this->allowedHosts);
 
+	/**
+	 * Return array of allowed hosts to proxy to. Used by the proxy.
+	 */
+	function getAllowedHosts() {
+		return $this->allowedHosts;
+	}
+	
+	function canAccess() {
+
+		/*	
+		    host = url.split("/")[2]
+		    if allowedHosts and not host in allowedHosts:
+		        print "Status: 502 Bad Gateway"
+		        print "Content-Type: text/plain"
+		        print
+		        print "This proxy does not allow you to access that location (%s)." % (host,)
+		        print
+		        print os.environ
+		*/
+		
+		throw new OpenLayersModel_Exception('not implemented yet');
 	}
 
 }
+
+class OpenLayersModel_Exception extends Exception {
+}
+
