6926a6547b65b31dc38f139d7c4b932d0fedd7c4
rspittel <rspittel@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-01-10 23:14:04 +0000

    ENHANCEMENTS: new unit tests
    git-svn-id: svn://svn.silverstripe.com/silverstripe/modules/openlayers/trunk@96556 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ tests/OpenLayersModelTest.php
@@ -0,0 +1,42 @@
+<?php
+
+class OpenLayersModelTest extends SapphireTest {
+	
+	
+	/**
+	 * Test getCMSFields  (basic test)
+	 */
+	function testgetRequiredJavaScript_InDev() {
+		Director::set_environment_type('dev');
+		
+		$obj = new OpenLayersModel();
+		$jscript = $obj->getRequiredJavaScript();		
+
+		$this->assertEquals($jscript, "openlayers/javascript/jsparty/lib/OpenLayers.js");
+	}
+
+	/**
+	 * Test getCMSFields  (basic test)
+	 */
+	function testgetRequiredJavaScript_InTest() {
+		Director::set_environment_type('test');
+		
+		$obj = new OpenLayersModel();
+		$jscript = $obj->getRequiredJavaScript();		
+
+		$this->assertEquals($jscript, "openlayers/javascript/jsparty/OpenLayers.js");
+	}
+
+	/**
+	 * Test getCMSFields  (basic test)
+	 */
+	function testgetRequiredJavaScript_InProd() {
+		Director::set_environment_type('live');
+		
+		$obj = new OpenLayersModel();
+		$jscript = $obj->getRequiredJavaScript();		
+
+		$this->assertEquals($jscript, "openlayers/javascript/jsparty/OpenLayers.js");
+	}
+
+}
