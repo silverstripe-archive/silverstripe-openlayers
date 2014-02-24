8cf6b803b7926e678d3f300ff7b26ee2144db528
Rainer Spittel <rainer@silverstripe.com>
2014-02-24 13:37:44 +1300

    ENHANCEMENT added backend javascript to request the column names from the server side for a layer.

--- javascript/ollayeradmin.js
+++ javascript/ollayeradmin.js
@@ -1,30 +1,59 @@
 (function($) {
-	$(document).ready(function() {
-		
-		$('.describeFeatureType').livequery('click', function() {
 
-			var LayerID = $('#Form_EditForm_ID').val();
-			var url = 'admin/openlayers/OLLayer/' + LayerID + '/describeFeature?SecurityID=' + $('#Form_EditForm_SecurityID').val();
-			var self = $(this);
+    $.entwine('openlayers', function($) {
 
-			$.ajax({ 
-				url: url ,
-				success: function( data ){
-					var obj = $("#featureTypeAttributes");
-					obj[0].innerHTML='<strong>Result:</strong><br/><ul>';
-					
-					var ar = eval(data);
-					$.each(ar, function( index, value) {
-						obj[0].innerHTML += "<li style='padding-left: 20px'>"+value+"</li>";
-					});
-					obj[0].innerHTML+='</ul>';
-				},
-				error: function(){
-					statusMessage('There was an error, please try again', 'bad');
-				}
-			});
-			return false;
-		});
-	});
+        $('#Form_ItemEditForm_UseTemplateForPopupWindow').entwine({
 
+            onmatch: function() {
+                this.updateForm();
+            },
+
+            onclick: function() {
+                this.updateForm();
+            },
+
+            updateForm: function() {
+                var checked = $(this)[0].checked;
+
+                var divAttributes = $('.divAttributesTemplate');
+                var divTemplate   = $('.divPopupTemplate');
+
+                if (checked == true) {
+                    divAttributes.hide();
+                    divTemplate.show();
+                } else {
+                    divAttributes.show();
+                    divTemplate.hide();
+                }
+            }
+
+        }),
+
+        $('.describeFeatureType').entwine({
+                onclick: function() {
+                    var item = $('.describeFeatureType');
+                    var layerID = $(item).data('id')
+                    var url = 'admin/wfs/describeFeature/' + layerID;
+
+                    $.ajax({
+                        url: url ,
+                        success: function( data ){
+                            var obj = $("#featureTypeAttributes");
+                            obj[0].innerHTML='<strong>Result:</strong><br/><ul>';
+
+                            var ar = eval(data);
+                            $.each(ar, function( index, value) {
+                                obj[0].innerHTML += "<li style='padding-left: 20px'>"+value+"</li>";
+                            });
+                            obj[0].innerHTML+='</ul>';
+                        },
+                        error: function(){
+                            statusMessage('There was an error, please try again', 'bad');
+                        }
+                    });
+                    return false;
+                }
+
+        });
+    });
 })(jQuery);
